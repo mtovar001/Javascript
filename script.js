0fca0aa7ee2b441b815aab9dae8d86f462aea4ef
Mildred <mtovar001@hotmail.com>
2023-07-11 20:52:22 -0400

    final changes

---
+++ script.js
@@ -0,0 +1,19 @@
+let studentFirstName = "Brandon";
+let studentLastName = "N";
+let grade1 = 53;
+let grade2 = 96;
+let grade3 = 65;
+let grade4 = 74;
+let passingGrade = 80;
+
+
+let ispassingGrade = true
+let average = ((grade1 + grade2 + grade3 + grade4)/4)
+
+studentLastName = "Newman";
+console.log (studentLastName)
+
+console.log (studentFirstName + studentLastName + "has an average grade " + average)
+
+
+
